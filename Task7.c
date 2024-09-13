56c67c619575489ee3dd4622a85f07f1ddf3a8cc
SulimanRaza <razasuliman935@gmail.com>
2024-09-13 20:14:33 +0500

    Add files via upload

---
+++ Task7.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+printf("                      _____________\n");
+printf("                     /             \\ \n");
+printf("                    /   /\\_/\\       \\ \n");
+printf("                   /   ( . . )  ^    \\ \n");
+printf("                  /     )   (  ((     \\ \n");
+printf("\"cat\"            |     ( > < )_))      | \n");
+printf("                  \\     -----         / \n");
+printf("                   \\    k24-0668     / \n");
+printf("                    \\               / \n");
+printf("                     \\_____________/ \n");
+}
