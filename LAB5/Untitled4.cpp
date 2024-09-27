b5787bbf9d830b0e4c49d5b9d1b4940d97382fff
SulimanRaza <razasuliman935@gmail.com>
2024-09-27 15:11:13 +0500

    Add files via upload

---
+++ LAB5/Untitled4.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	int age,city;
+	printf("Enter your age");
+	scanf("%d",&age);
+	printf("Are you a citizen of pakistan (1 for yes, 2 for no)");
+	scanf(" %c",&city);
+	if(city==1){
+		if(age>=18)
+		printf("You are eligible to vote");
+		else
+		printf("you are not eligible to vote");
+	}
+	else
+	printf("you are not eligible to vote");
+	return 0;
+}
